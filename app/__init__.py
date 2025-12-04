930c5c5f775ad21ef68a41d360d41d58ecc491ec
christelgo <christelgohxl@gmail.com>
2025-12-04 09:34:17 +0800

    fixed some routing issues

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,21 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
-#from app.routes.chatbot_routes import chatbot_bp
-#from app.routes.menu_routes import menu_bp
 
 db = SQLAlchemy()
 
 def create_app():
     app= Flask(__name__)
-    app.config.from_object('app.config.Config')
+    app.config.from_object(Config)
     db.init_app(app)
 
     from app.routes.chatbot_routes import chatbot_bp
     from app.routes.menu_routes import menu_bp
+    from app.routes.order_routes import order_bp
 
-    app.register_blueprint(chatbot_bp, url_prefix='/api')
-    app.register_blueprint(menu_bp, url_prefix='/api')
+    app.register_blueprint(chatbot_bp, url_prefix='/api/chat')
+    app.register_blueprint(menu_bp, url_prefix='/api/menu')
+    app.register_blueprint(order_bp,url_prefix='/api/orders' )
+    
 # Note for later: I wanna get rid of the /api as I feel like it is redundant
     return app
--- app/routes/chatbot_routes.py
+++ app/routes/chatbot_routes.py
@@ -1,14 +1,12 @@
 from flask import Blueprint, request, jsonify
 from app.services.bot_engine import generate_bot_reply
 
-chatbot_bp = Blueprint('chatbot', __name__)
+chatbot_bp = Blueprint('chatbot_bp', __name__)
 
-@chatbot_bp.route("/chat", methods = ["POST"])
+@chatbot_bp.route("/", methods = ["POST"])
 def chat():
     data = request.get_json()
-
     user_message = data.get("message","")
-
     reply = generate_bot_reply(user_message)
     
     return jsonify({"reply":reply})
